c185a8d4bb55b0cb502b2c7db1eb1161f4feb448
creat4869 <silence.24yue@gmail.com>
2019-07-18 00:32:13 +0900

    work-page_front__index_show

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,24 @@
+class WorksController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+  
+end
